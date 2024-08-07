78788cb8ff0371ee4cd33a7168bcd399f096d292
kiraken <148177084+Kiraken-cmd@users.noreply.github.com>
2024-08-07 15:52:14 +0700

    script.js

--- js/script.js
+++ js/script.js
@@ -12,3 +12,47 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+
+// Memfungsikan menu contact me agar terkirim ke email
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("form");
+  const submitButton = form.querySelector(".btn");
+  const inputs = form.querySelectorAll("input[required], textarea[required]");
+
+  // Function untuk check validitas form
+  function checkFormValidity() {
+    let isValid = true;
+    inputs.forEach((input) => {
+      if (!input.value.trim()) {
+        isValid = false;
+      }
+    });
+    submitButton.disabled = !isValid;
+  }
+
+  // Tambahkan event listener ke input
+  inputs.forEach((input) => {
+    input.addEventListener("input", checkFormValidity);
+  });
+
+  // Pemeriksaan awal pada pemuatan halaman
+  checkFormValidity();
+
+  // Menangani pengiriman form
+  form.addEventListener("submit", function (event) {
+    if (!submitButton.disabled) {
+      // Izinkan pengiriman form
+      form.submit();
+      alert(
+        "Your message has been successfully sent!, Thank you for your feedback 😊"
+      );
+      // Tunda pemuatan ulang untuk memastikan permintaan pengiriman form terkirim
+      setTimeout(() => {
+        location.reload();
+      }, 1000);
+    } else {
+      // Cegah pengiriman formulir jika form tidak valid
+      event.preventDefault();
+    }
+  });
+});
