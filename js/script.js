7ee9b16e2573309ad1e846f4ec7ee57c2c0a5ac6
unknown <kiraken001@gmail.com>
2024-09-09 15:35:29 +0700

    update

--- js/script.js
+++ js/script.js
@@ -101,10 +101,14 @@ navbarLinks.forEach((link) => {
 });
 
 // Url Config
-if (window.location.pathname.endsWith("index.html")) {
-  window.history.pushState(
-    {},
-    "",
-    window.location.pathname.replace("index.html", "")
-  );
+// Mendapatkan path dari URL saat ini
+let currentPath = window.location.pathname;
+
+// Mengecek apakah path diakhiri dengan ".html"
+if (currentPath.endsWith("index.html")) {
+  // Menghapus ".html" dari akhir path
+  let newPath = currentPath.slice(0, -5);
+
+  // Mengubah URL tanpa memuat ulang halaman
+  window.history.replaceState(null, "", newPath);
 }
